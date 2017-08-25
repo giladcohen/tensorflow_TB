6fecb08c983f2ba96bc61c87f5523ccbd5926be2
giladcohen <giladadlig@gmail.com>
2017-08-25 03:29:30 +0300

    fixing small bugs

--- lib/preprocessors/preprocessor.py
+++ lib/preprocessors/preprocessor.py
@@ -11,32 +11,28 @@ class PreProcessor(PreProcessorBase):
         # There is no need for label augmentation for classification task
         # Therefore self.label_augmentation is not used here. It will be used for preprocessing
         # other tasks such as detection/segmentation
-        if self.data_augmentation:
-            images_aug = np.empty(images.shape, dtype=np.uint8)
-            labels_aug = np.empty(labels.shape, dtype=np.int)
-            for i in xrange(images.shape[0]):
-                image = images[i]
-                label = labels[i]
-                min_drift_x = -self.drift_x
-                max_drift_x =  self.drift_x
-                min_drift_y = -self.drift_y
-                max_drift_y =  self.drift_y
-                dy = self.rand_gen.randint(min_drift_y, max_drift_y + 1)
-                dx = self.rand_gen.randint(min_drift_x, max_drift_x + 1)
+        images_aug = np.empty(images.shape, dtype=np.uint8)
+        labels_aug = np.empty(labels.shape, dtype=np.int)
+        for i in xrange(images.shape[0]):
+            image = images[i]
+            label = labels[i]
+            min_drift_x = -self.drift_x
+            max_drift_x =  self.drift_x
+            min_drift_y = -self.drift_y
+            max_drift_y =  self.drift_y
+            dy = self.rand_gen.randint(min_drift_y, max_drift_y + 1)
+            dx = self.rand_gen.randint(min_drift_x, max_drift_x + 1)
 
-                orig_x, dist_x = max(dx, 0), max(-dx, 0)
-                orig_y, dist_y = max(dy, 0), max(-dy, 0)
-                distorted_im = np.zeros([H, W, D])
-                distorted_im[dist_y:H - orig_y, dist_x:W - orig_x, :] = image[orig_y:H - dist_y, orig_x:W - dist_x, :]
-                image = distorted_im
+            orig_x, dist_x = max(dx, 0), max(-dx, 0)
+            orig_y, dist_y = max(dy, 0), max(-dy, 0)
+            distorted_im = np.zeros([H, W, D])
+            distorted_im[dist_y:H - orig_y, dist_x:W - orig_x, :] = image[orig_y:H - dist_y, orig_x:W - dist_x, :]
+            image = distorted_im
 
-                if self.flip_image and self.rand_gen.randint(2) > 0.5:
-                    image = image[:, ::-1, :]
+            if self.flip_image and self.rand_gen.randint(2) > 0.5:
+                image = image[:, ::-1, :]
 
-                images_aug[i] = image
-                labels_aug[i] = label  # for classification task
-        else:
-            images_aug = images
-            labels_aug = labels
+            images_aug[i] = image
+            labels_aug[i] = label  # for classification task
 
         return images_aug, labels_aug
--- lib/preprocessors/preprocessor_base.py
+++ lib/preprocessors/preprocessor_base.py
@@ -9,8 +9,6 @@ class PreProcessorBase(AgentBase):
         super(PreProcessorBase, self).__init__(name)
         self.prm = prm
         self.preprocessor       = self.prm.network.pre_processing.PREPROCESSOR
-        self.data_augmentation  = self.prm.train.data_augmentation.DATA_AUGMENTATION
-        self.label_augmentation = self.prm.train.data_augmentation.LABEL_AUGMENTATION
         self.flip_image         = self.prm.train.data_augmentation.FLIP_IMAGE
         self.drift_x            = self.prm.train.data_augmentation.DRIFT_X
         self.drift_y            = self.prm.train.data_augmentation.DRIFT_Y
@@ -23,17 +21,11 @@ class PreProcessorBase(AgentBase):
         """print basic preprocessor parameters"""
         self.log.info('PreProcessor parameters:')
         self.log.info(' PREPROCESSOR: {}'.format(self.preprocessor))
-        self.log.info(' DATA_AUGMENTATION: {}'.format(self.data_augmentation))
-        self.log.info(' LABEL_AUGMENTATION: {}'.format(self.label_augmentation))
         self.log.info(' FLIP_IMAGE: {}'.format(self.flip_image))
         self.log.info(' DRIFT_X: {}'.format(self.drift_x))
         self.log.info(' DRIFT_Y: {}'.format(self.drift_y))
 
     def assert_config(self):
-        if self.label_augmentation and not self.data_augmentation:
-            err_str = 'assert_config: LABEL_AUGMENTATION cannot be set to True with DATA_AUGMENTATION=False'
-            self.log.error(err_str)
-            raise ValueError(err_str)
         if self.drift_x < 0 or self.drift_y < 0:
             err_str = 'assert_config: DRIFT_X ({}) and DRIFT_Y ({}) should both be positive'.format(self.drift_x, self.drift_y)
             self.log.error(err_str)
