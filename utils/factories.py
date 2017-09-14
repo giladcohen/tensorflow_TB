ac9a79ccce71f169e6cc40197c5d7bdb43a96aef
giladcohen <giladadlig@gmail.com>
2017-09-14 09:01:24 +0300

    adding balanced uncertained trainer

--- utils/factories.py
+++ utils/factories.py
@@ -7,6 +7,7 @@ from lib.trainers.random_sampler_trainer import RandomSamplerTrainer
 from lib.trainers.all_centers_trainer import AllCentersTrainer
 from lib.trainers.class_centers_trainer import ClassCentersTrainer
 from lib.trainers.most_uncertained_trainer import MostUncertainedTrainer
+from lib.trainers.most_uncertained_balanced_trainer import MostUncertainedBalancedTrainer
 from lib.trainers.hooks.learning_rate_setter_base import LearningRateSetterBase
 from lib.trainers.hooks.fixed_schedule_setter import FixedScheduleSetter
 from lib.trainers.hooks.decay_by_score_setter import DecayByScoreSetter
@@ -76,11 +77,12 @@ class Factories(object):
             raise AssertionError(err_str)
 
     def get_trainer(self, model, dataset):
-        available_trainers = {'simple'           : ClassificationTrainer,
-                              'random_sampler'   : RandomSamplerTrainer,
-                              'all_centers'      : AllCentersTrainer,
-                              'class_centers'    : ClassCentersTrainer,
-                              'most_uncertained' : MostUncertainedTrainer}
+        available_trainers = {'simple'                    : ClassificationTrainer,
+                              'random_sampler'            : RandomSamplerTrainer,
+                              'all_centers'               : AllCentersTrainer,
+                              'class_centers'             : ClassCentersTrainer,
+                              'most_uncertained'          : MostUncertainedTrainer,
+                              'most_uncertained_balanced' : MostUncertainedBalancedTrainer}
         if self.trainer in available_trainers:
             trainer = available_trainers[self.trainer](self.trainer, self.prm, model, dataset)
             self.log.info('get_trainer: returning ' + str(trainer))
