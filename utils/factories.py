5f4b5d1077a00ef5819397a749fd4fc4393061c1
giladcohen <giladadlig@gmail.com>
2017-11-13 03:06:58 +0200

    adding kmeans on most uncertained (trainer)

--- utils/factories.py
+++ utils/factories.py
@@ -10,6 +10,7 @@ from lib.trainers.most_uncertained_trainer import MostUncertainedTrainer
 from lib.trainers.most_uncertained_balanced_trainer import MostUncertainedBalancedTrainer
 from lib.trainers.cross_entropy_trainer import CrossEntropyTrainer
 from lib.trainers.cross_entropy_trainer2 import CrossEntropyTrainer2
+from lib.trainers.kmeans_on_most_uncertained_trainer import KMeansOnMostUncertainedTrainer
 from lib.trainers.hooks.learning_rate_setter_base import LearningRateSetterBase
 from lib.trainers.hooks.fixed_schedule_setter import FixedScheduleSetter
 from lib.trainers.hooks.decay_by_score_setter import DecayByScoreSetter
@@ -77,14 +78,15 @@ class Factories(object):
             raise AssertionError(err_str)
 
     def get_trainer(self, model, dataset):
-        available_trainers = {'simple'                    : ClassificationTrainer,
-                              'random_sampler'            : RandomSamplerTrainer,
-                              'all_centers'               : AllCentersTrainer,
-                              'class_centers'             : ClassCentersTrainer,
-                              'most_uncertained'          : MostUncertainedTrainer,
-                              'most_uncertained_balanced' : MostUncertainedBalancedTrainer,
-                              'cross_entropy'             : CrossEntropyTrainer,
-                              'cross_entropy2'            : CrossEntropyTrainer2}
+        available_trainers = {'simple'                     : ClassificationTrainer,
+                              'random_sampler'             : RandomSamplerTrainer,
+                              'all_centers'                : AllCentersTrainer,
+                              'class_centers'              : ClassCentersTrainer,
+                              'most_uncertained'           : MostUncertainedTrainer,
+                              'most_uncertained_balanced'  : MostUncertainedBalancedTrainer,
+                              'cross_entropy'              : CrossEntropyTrainer,
+                              'cross_entropy2'             : CrossEntropyTrainer2,
+                              'kmeans_on_most_uncertained' : KMeansOnMostUncertainedTrainer}
         if self.trainer in available_trainers:
             trainer = available_trainers[self.trainer](self.trainer, self.prm, model, dataset)
             self.log.info('get_trainer: returning ' + str(trainer))
