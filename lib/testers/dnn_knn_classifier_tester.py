39cd3151038b6c343a34c85a815cf6ddc3c3f943
giladcohen <giladadlig@gmail.com>
2018-01-14 20:44:51 +0200

    fix typo

--- lib/testers/dnn_knn_classifier_tester.py
+++ lib/testers/dnn_knn_classifier_tester.py
@@ -20,7 +20,7 @@ class DNNKNNClassifierTester(KNNClassifierTester):
         train_labels_file              = os.path.join(self.test_dir, 'train_labels.npy')
         test_labels_file               = os.path.join(self.test_dir, 'test_labels.npy')
 
-        if self.load_from_disc:
+        if self.load_from_disk:
             self.log.info('Loading {}/{} train/test set embedding features from disc'.format(train_size, test_size))
             X_train_features          = np.load(train_features_file)
             y_train                   = np.load(train_labels_file)
