d3cccab87f2e3122394a68c0cc42c4229918916e
giladcohen <giladadlig@gmail.com>
2018-01-31 16:40:39 +0200

    updating knn classifier

--- lib/testers/knn_classifier_tester.py
+++ lib/testers/knn_classifier_tester.py
@@ -39,10 +39,8 @@ class KNNClassifierTester(TesterBase):
             p=int(self.knn_norm[-1]),
             n_jobs=self.knn_jobs)
 
-    def test(self):
-        train_size = self.dataset.train_set_size
-        test_size  = self.dataset.test_set_size
-
+    def fetch_dump_data_features(self):
+        """Optionally fetching precomputed train/test features, and labels."""
         train_features_file            = os.path.join(self.test_dir, 'train_features.npy')
         test_features_file             = os.path.join(self.test_dir, 'test_features.npy')
         test_dnn_predictions_prob_file = os.path.join(self.test_dir, 'test_dnn_predictions_prob.npy')
@@ -50,14 +48,14 @@ class KNNClassifierTester(TesterBase):
         test_labels_file               = os.path.join(self.test_dir, 'test_labels.npy')
 
         if self.load_from_disk:
-            self.log.info('Loading {}/{} train/test set embedding features from disk'.format(train_size, test_size))
+            self.log.info('Loading {}/{} train/test set embedding features from disk'.format(self.dataset.train_set_size, self.dataset.test_set_size))
             X_train_features          = np.load(train_features_file)
             y_train                   = np.load(train_labels_file)
             X_test_features           = np.load(test_features_file)
             y_test                    = np.load(test_labels_file)
             test_dnn_predictions_prob = np.load(test_dnn_predictions_prob_file)
         else:
-            self.log.info('Collecting {} train set embedding features'.format(train_size))
+            self.log.info('Collecting {} train set embedding features'.format(self.dataset.train_set_size))
             (X_train_features, y_train) = \
                 collect_features(
                     agent=self,
@@ -65,13 +63,14 @@ class KNNClassifierTester(TesterBase):
                     fetches=[self.model.net['embedding_layer'], self.model.labels],
                     feed_dict={self.model.dropout_keep_prob: 1.0})
 
-            self.log.info('Collecting {} test set embedding features and DNN predictions'.format(test_size))
+            self.log.info('Collecting {} test set embedding features and DNN predictions'.format(self.dataset.test_set_size))
             (X_test_features, y_test, test_dnn_predictions_prob) = \
                 collect_features(
                     agent=self,
                     dataset_type='test',
                     fetches=[self.model.net['embedding_layer'], self.model.labels, self.model.predictions_prob],
                     feed_dict={self.model.dropout_keep_prob: 1.0})
+
         if self.dump_net:
             self.log.info('Dumping train features into disk:\n{}\n{}\n{}\n{}\n{}'
                           .format(train_features_file, test_features_file, test_dnn_predictions_prob_file, train_labels_file, test_labels_file))
@@ -80,6 +79,15 @@ class KNNClassifierTester(TesterBase):
             np.save(test_dnn_predictions_prob_file, test_dnn_predictions_prob)
             np.save(train_labels_file             , y_train)
             np.save(test_labels_file              , y_test)
+
+        return X_train_features, X_test_features, test_dnn_predictions_prob, y_train, y_test
+
+    def test(self):
+        X_train_features, \
+        X_test_features, \
+        test_dnn_predictions_prob, \
+        y_train, \
+        y_test = self.fetch_dump_data_features()
 
         if self.pca_reduction:
             self.log.info('Reducing features_vec from {} dims to {} dims using PCA'.format(self.model.embedding_dims, self.pca_embedding_dims))
@@ -92,36 +100,23 @@ class KNNClassifierTester(TesterBase):
         self.log.info('Fitting KNN model...')
         self.knn.fit(X_train_features_post, y_train)
 
-        self.log.info('Predicting test set labels from KNN model...')
-        test_knn_predictions_prob = self.knn.predict_proba(X_test_features_post)
+        if self.decision_method == 'dnn_accuracy':
+            y_pred = test_dnn_predictions_prob.argmax(axis=1)
+        elif self.decision_method == 'knn_accuracy':
+            self.log.info('Predicting precomputed test set labels from KNN model...')
+            test_knn_predictions_prob = self.knn.predict_proba(X_test_features_post)
+            y_pred = test_knn_predictions_prob.argmax(axis=1)
 
-        # calculating only one prediction for every feature vector
-        test_dnn_pred = test_dnn_predictions_prob.argmax(axis=1)
-        test_knn_pred = test_knn_predictions_prob.argmax(axis=1)
-
-        # calculating different metrics
-        dnn_accuracy  = np.sum(test_dnn_pred==y_test)/test_size
-        knn_accuracy  = np.sum(test_knn_pred==y_test)/test_size
-        dnn_nmi_score = normalized_mutual_info_score(labels_true=y_test, labels_pred=test_dnn_pred)
-        knn_nmi_score = normalized_mutual_info_score(labels_true=y_test, labels_pred=test_knn_pred)
+        accuracy = np.sum(y_pred==y_test)/self.dataset.test_set_size
 
         # writing summaries
-        self.tb_logger_test.log_scalar('score_metrics/dnn_accuracy', dnn_accuracy , self.global_step)
-        self.tb_logger_test.log_scalar('score_metrics/dnn_NMI'     , dnn_nmi_score, self.global_step)
-
-        score_str = 'score_metrics/K={}/PCA={}/norm={}/weights={}'\
-            .format(self.knn_neighbors, self.pca_embedding_dims, self.knn_norm, self.knn_weights)
-        self.tb_logger_test.log_scalar(score_str + '/knn_accuracy', knn_accuracy , self.global_step)
-        self.tb_logger_test.log_scalar(score_str + '/knn_NMI'     , knn_nmi_score, self.global_step)
-        print_str = '{}: knn_accuracy= {}, knn_NMI={}'.format(score_str, knn_accuracy, knn_nmi_score)
+        score_str = 'score_metrics/K={}/PCA={}/norm={}/weights={}/decision_method={}'\
+            .format(self.knn_neighbors, self.pca_embedding_dims, self.knn_norm, self.knn_weights, self.decision_method)
+        self.tb_logger_test.log_scalar(score_str, accuracy, self.global_step)
+        print_str = '{}: accuracy= {}'.format(score_str, accuracy)
         self.log.info(print_str)
         print(print_str)
         self.summary_writer_test.flush()
-
-        print_str = 'TEST : dnn accuracy: {}, dnn NMI score: {}\n\t   knn accuracy: {} knn NMI score: {}'\
-            .format(dnn_accuracy, dnn_nmi_score, knn_accuracy, knn_nmi_score)
-        self.log.info(score_str)
-        print(print_str)
 
         self.log.info('Tester {} is done'.format(str(self)))
 
