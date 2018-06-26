8c7645778cb452a438f2e1b9e36d258520cef00b
giladcohen <giladadlig@gmail.com>
2018-06-26 10:44:45 +0300

    fix knn accuracy plots

--- plots/knn_accuracy_plots.py
+++ plots/knn_accuracy_plots.py
@@ -7,25 +7,27 @@ import numpy as np
 fig = plt.figure(figsize=(15.0, 8.0))
 subpos = [0.55, 0.3, 0.4, 0.3]
 # wrn, mnist
-csv_file = '/data/gilad/logs/ma_scores/wrn/mnist/log_2238_210318_ma_score_wrn_mnist_wd_0.00078-SUPERSEED=21031802/data_for_figures/test___knn_score'
+# csv_file = '/data/gilad/logs/ma_scores/wrn/mnist/log_2238_210318_ma_score_wrn_mnist_wd_0.00078-SUPERSEED=21031802/data_for_figures/test___knn_score'
+csv_file = '/data/gilad/logs/ma_scores/wrn/mnist/log_0746_020518_ma_score_wrn_mnist_wd_0.00078_steps_1000-SUPERSEED=21031802/data_for_figures/test___knn_score'
 steps, values = load_data_from_csv_wrapper(csv_file)
 ax1 = fig.add_subplot(331)
 ax1.plot(steps, values, 'r')
 subax1 = add_subplot_axes(ax1, subpos)
-subax1.set_ylim([99, 99.5])
-subax1.set_yticks([99, 99.5])
-subax1.plot(steps[-11:], values[-11:], 'r')
-csv_file = '/data/gilad/logs/ma_scores/wrn/mnist/log_2238_210318_ma_score_wrn_mnist_wd_0.00078-SUPERSEED=21031802/data_for_figures/test___score'
+subax1.set_ylim([99.3, 99.6])
+subax1.set_yticks([99.3, 99.6])
+subax1.plot(steps[-21:], values[-21:], 'r')
+# csv_file = '/data/gilad/logs/ma_scores/wrn/mnist/log_2238_210318_ma_score_wrn_mnist_wd_0.00078-SUPERSEED=21031802/data_for_figures/test___score'
+csv_file = '/data/gilad/logs/ma_scores/wrn/mnist/log_0746_020518_ma_score_wrn_mnist_wd_0.00078_steps_1000-SUPERSEED=21031802/data_for_figures/test___score'
 steps, values = load_data_from_csv_wrapper(csv_file)
 ax1.plot(steps, values, 'black')
-subax1.plot(steps[-11:], values[-11:], 'black')
+subax1.plot(steps[-21:], values[-21:], 'black')
 ax1.set_ylim(bottom=0, top=110)
 ax1.set_title('MNIST')
-ax1.text(-180, 52, 'Wide ResNet 28-10', va='center', rotation='vertical', fontdict={'fontsize': 13})
+ax1.text(-360, 52, 'Wide ResNet 28-10', va='center', rotation='vertical', fontdict={'fontsize': 13})
 ax1.yaxis.grid()
 ax1.set_ylabel('accuracy (%)', labelpad=0.5, fontdict={'fontsize': 12})
-ax1.add_patch(patches.Polygon(xy=np.array([[400, 104], [278, 66.2], [500, 66.2], [500, 32.2], [500, 94.2]]), closed=True, color='silver'))
-ax1.add_patch(patches.Rectangle(xy=(400, 95), width=100, height=10, facecolor='moccasin'))
+ax1.add_patch(patches.Polygon(xy=np.array([[800, 104], [556, 66.2], [1000, 66.2], [1000, 32.2], [1000, 94.2]]), closed=True, color='silver'))
+ax1.add_patch(patches.Rectangle(xy=(800, 95), width=200, height=10, facecolor='moccasin'))
 
 # wrn, cifar10
 csv_file = '/data/gilad/logs/ma_scores/wrn/cifar10/log_1444_070318_wrn_ma_score_wd_0.00078-SUPERSEED=07031800/data_for_figures/test___knn_score'
@@ -66,24 +68,26 @@ ax3.add_patch(patches.Polygon(xy=np.array([[40000, 83.1], [27700, 50.75], [50000
 ax3.add_patch(patches.Rectangle(xy=(40000, 75), width=10000, height=8, facecolor='moccasin'))
 
 # lenet, mnist
-csv_file = '/data/gilad/logs/ma_scores/lenet/mnist/log_2200_100318_ma_score_lenet_mnist_wd_0.0-SUPERSEED=10031800/data_for_figures/test___knn_score'
+# csv_file = '/data/gilad/logs/ma_scores/lenet/mnist/log_2200_100318_ma_score_lenet_mnist_wd_0.0-SUPERSEED=10031800/data_for_figures/test___knn_score'
+csv_file = '/data/gilad/logs/ma_scores/lenet/mnist/log_0746_020518_ma_score_lenet_mnist_wd_0.0_steps_1000-SUPERSEED=10031800/data_for_figures/test___knn_score'
 steps, values = load_data_from_csv_wrapper(csv_file)
 ax4 = fig.add_subplot(334)
 ax4.plot(steps, values, 'r')
 subax4 = add_subplot_axes(ax4, subpos)
 subax4.set_ylim([98.7, 99.2])
 subax4.set_yticks([98.7, 99.2])
-subax4.plot(steps[-11:], values[-11:], 'r')
-csv_file = '/data/gilad/logs/ma_scores/lenet/mnist/log_2200_100318_ma_score_lenet_mnist_wd_0.0-SUPERSEED=10031800/data_for_figures/test___score'
+subax4.plot(steps[-21:], values[-21:], 'r')
+# csv_file = '/data/gilad/logs/ma_scores/lenet/mnist/log_2200_100318_ma_score_lenet_mnist_wd_0.0-SUPERSEED=10031800/data_for_figures/test___score'
+csv_file = '/data/gilad/logs/ma_scores/lenet/mnist/log_0746_020518_ma_score_lenet_mnist_wd_0.0_steps_1000-SUPERSEED=10031800/data_for_figures/test___score'
 steps, values = load_data_from_csv_wrapper(csv_file)
 ax4.plot(steps, values, 'black')
-subax4.plot(steps[-11:], values[-11:], 'black')
+subax4.plot(steps[-21:], values[-21:], 'black')
 ax4.set_ylim(bottom=0, top=110)
-ax4.text(-180, 52, 'LeNet', va='center', rotation='vertical', fontdict={'fontsize': 13})
+ax4.text(-360, 52, 'LeNet', va='center', rotation='vertical', fontdict={'fontsize': 13})
 ax4.yaxis.grid()
 ax4.set_ylabel('accuracy (%)', labelpad=0.5, fontdict={'fontsize': 12})
-ax4.add_patch(patches.Polygon(xy=np.array([[400, 103.5], [278, 65.9], [500, 65.9], [500, 33.75], [500, 94.5]]), closed=True, color='silver'))
-ax4.add_patch(patches.Rectangle(xy=(400, 94), width=100, height=10, facecolor='moccasin'))
+ax4.add_patch(patches.Polygon(xy=np.array([[800, 103.5], [556, 65.9], [1000, 65.9], [1000, 33.75], [1000, 94.5]]), closed=True, color='silver'))
+ax4.add_patch(patches.Rectangle(xy=(800, 94), width=200, height=10, facecolor='moccasin'))
 
 # lenet, cifar10
 csv_file = '/data/gilad/logs/ma_scores/lenet/cifar10/log_2354_060318_lenet_ma_score_wd_0.008-SUPERSEED=06031800/data_for_figures/test___knn_score'
@@ -123,15 +127,17 @@ ax6.add_patch(patches.Polygon(xy=np.array([[40000, 56.2], [27800, 35.6], [50000,
 ax6.add_patch(patches.Rectangle(xy=(40000, 50), width=10000, height=6, facecolor='moccasin'))
 
 # fc2net, mnist
-csv_file = '/data/gilad/logs/ma_scores/fc2net/mnist/log_1409_140318_ma_score_fc2net_mnist_wd_0.0-SUPERSEED=14031800/data_for_figures/test___knn_score'
+# csv_file = '/data/gilad/logs/ma_scores/fc2net/mnist/log_1409_140318_ma_score_fc2net_mnist_wd_0.0-SUPERSEED=14031800/data_for_figures/test___knn_score'
+csv_file = '/data/gilad/logs/ma_scores/fc2net/mnist/log_0746_020518_ma_score_fc2net_mnist_wd_0.0_steps_1000-SUPERSEED=10031800/data_for_figures/test___knn_score'
 steps, values = load_data_from_csv_wrapper(csv_file)
 ax7 = fig.add_subplot(337)
 ax7.plot(steps, values, 'r')
-csv_file = '/data/gilad/logs/ma_scores/fc2net/mnist/log_1409_140318_ma_score_fc2net_mnist_wd_0.0-SUPERSEED=14031800/data_for_figures/test___score'
+# csv_file = '/data/gilad/logs/ma_scores/fc2net/mnist/log_1409_140318_ma_score_fc2net_mnist_wd_0.0-SUPERSEED=14031800/data_for_figures/test___score'
+csv_file = '/data/gilad/logs/ma_scores/fc2net/mnist/log_0746_020518_ma_score_fc2net_mnist_wd_0.0_steps_1000-SUPERSEED=10031800/data_for_figures/test___score'
 steps, values = load_data_from_csv_wrapper(csv_file)
 ax7.plot(steps, values, 'black')
 ax7.set_ylim(bottom=0, top=110)
-ax7.text(-180, 52, 'MLP-640', va='center', rotation='vertical', fontdict={'fontsize': 13})
+ax7.text(-360, 52, 'MLP-640', va='center', rotation='vertical', fontdict={'fontsize': 13})
 ax7.yaxis.grid()
 ax7.set_ylabel('accuracy (%)', labelpad=0.5, fontdict={'fontsize': 12})
 
@@ -167,4 +173,4 @@ ax9.legend(['k-NN', 'DNN'], loc=(1.05, 3.1))
 
 # plt.show()
 
-# plt.savefig('knn_dnn_acuuracy_vs_iter.png', dpi=350)
+plt.savefig('knn_dnn_accuracy_vs_iter_1000_steps.png', dpi=350)
