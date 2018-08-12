5718ccc1c7841cec9808aab69e29bdc78d6fb26c
giladcohen <giladadlig@gmail.com>
2018-08-12 03:29:25 +0300

    compact code

--- lib/trainers/classification_metrics_trainer.py
+++ lib/trainers/classification_metrics_trainer.py
@@ -6,13 +6,13 @@ from lib.trainers.classification_trainer import ClassificationTrainer
 import numpy as np
 from utils.misc import collect_features, calc_mutual_agreement, calc_psame
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
 # from skl_groups.divergences import KNNDivergenceEstimator
 from scipy.stats import entropy
 
-eps = 0.0001
+eps = 0.000001
 
 class ClassificationMetricsTrainer(ClassificationTrainer):
     """Implementing classification trainer with many different metrics"""
@@ -23,10 +23,10 @@ class ClassificationMetricsTrainer(ClassificationTrainer):
         self.pca_embedding_dims    = self.prm.train.train_control.PCA_EMBEDDING_DIMS
 
         self.randomized_dataset = False
-        self.eval_trainset      = False
+        self.eval_trainset      = True
         self.collect_knn        = True
-        self.collect_svm        = False
-        self.collect_lr         = False
+        self.collect_svm        = True
+        self.collect_lr         = True
 
         if self.randomized_dataset:
             self.train_handle = 'train_random'
@@ -58,9 +58,9 @@ class ClassificationMetricsTrainer(ClassificationTrainer):
             p=int(self.knn_norm[-1]),
             n_jobs=self.knn_jobs)
 
-        self.svm = LinearSVC(
-            penalty=self.knn_norm.lower(),
-            dual=False,
+        self.svm = SVC(
+            kernel='linear',
+            probability=True,
             random_state=self.rand_gen)
 
         self.lr = LogisticRegression(
@@ -95,7 +95,7 @@ class ClassificationMetricsTrainer(ClassificationTrainer):
         :return: knn predictions of the training set, using an efficient leave-one-out.
         """
         biased_knn_predictions_prob_train = self.knn_train.predict_proba(X_train_features)
-        knn_predictions_prob_train = np.zeros(y_train.shape)
+        knn_predictions_prob_train = np.zeros(biased_knn_predictions_prob_train.shape)
 
         for i in range(len(X_train_features)):
             y = int(y_train[i])
@@ -108,6 +108,65 @@ class ClassificationMetricsTrainer(ClassificationTrainer):
             knn_predictions_prob_train[i] = proba
 
         return knn_predictions_prob_train
+
+    def process(self, model_name, dataset_name, X, y, dnn_predictions_prob):
+        """
+        :param model_name: A fitted model name to predict and save metrics for
+        :param dataset_name: 'test' or 'train'
+        :param X: dataset, features.
+        :param y: labels
+        :param dnn_predictions_prob: dnn predictions on the dataset
+        :return: None. Saves metrics.
+        """
+
+        if model_name is 'knn':
+            if dataset_name is 'test':
+                model = self.knn
+            else:
+                model = self.knn_train
+        elif model_name is 'svm':
+            model = self.svm
+        elif model_name is 'lr':
+            model = self.svm
+        else:
+            err_str = 'unknown model_name: {}'.format(model_name)
+            self.log.error(err_str)
+            raise AssertionError(err_str)
+
+        y_pred_dnn = dnn_predictions_prob.argmax(axis=1)
+
+        self.log.info('Predicting {} labels for dataset {} using model\n {}...'.format(y.shape[0], dataset_name, str(model)))
+        predictions_prob = model.predict_proba(X)
+        y_pred = predictions_prob.argmax(axis=1)
+
+        # calculate metrics
+        self.log.info('Calculate {} set scores...'.format(dataset_name))
+        score = np.average(y == y_pred)
+
+        self.log.info('Calculate ma/md and psame scores...')
+        ma_score, md_score = calc_mutual_agreement(y_pred_dnn, y_pred, y)
+        psame = calc_psame(y_pred_dnn, y_pred)
+
+        self.log.info('Calculate confidence scores...')
+
+        self.log.info('Calculate KL divergences...')
+        np.place(predictions_prob, predictions_prob == 0.0, [eps])
+        kl_div  = entropy(dnn_predictions_prob, predictions_prob)
+        kl_div2 = entropy(predictions_prob, dnn_predictions_prob)
+        kl_div_avg  = np.average(kl_div)
+        kl_div2_avg = np.average(kl_div2)
+
+        if dataset_name is 'test':
+            suffix = ''
+        else:
+            suffix = '_trainset'
+        self.tb_logger_test.log_scalar(model_name + '_score'       + suffix, score      , self.global_step)
+        self.tb_logger_test.log_scalar(model_name + '_ma_score'    + suffix, ma_score   , self.global_step)
+        self.tb_logger_test.log_scalar(model_name + '_md_score'    + suffix, md_score   , self.global_step)
+        self.tb_logger_test.log_scalar(model_name + '_psame'       + suffix, psame      , self.global_step)
+        self.tb_logger_test.log_scalar(model_name + '_kl_div_avg'  + suffix, kl_div_avg , self.global_step)
+        self.tb_logger_test.log_scalar(model_name + '_kl_div2_avg' + suffix, kl_div2_avg, self.global_step)
+
 
     def test_step(self):
         '''Implementing one test step.'''
@@ -142,172 +201,40 @@ class ClassificationMetricsTrainer(ClassificationTrainer):
             self.log.info('Fitting Logistic Regression model...')
             self.lr.fit(X_train_features, y_train)
 
-        # predictions (test set)
         self.log.info('Predicting test set labels from DNN model...')
         y_pred_dnn = test_dnn_predictions_prob.argmax(axis=1)
-        if self.collect_knn:
-            self.log.info('Predicting test set labels from KNN model...')
-            test_knn_predictions_prob = self.knn.predict_proba(X_test_features)
-            y_pred_knn = test_knn_predictions_prob.argmax(axis=1)
-        if self.collect_svm:
-            self.log.info('Predicting test set labels from SVM model...')
-            y_pred_svm = self.svm.predict(X_test_features)
-        if self.collect_lr:
-            self.log.info('Predicting test set labels from Logistic Regression model...')
-            test_lr_predictions_prob = self.lr.predict_proba(X_test_features)
-            y_pred_lr = test_lr_predictions_prob.argmax(axis=1)
-
-        # calculate metrics
-        self.log.info('Calculate test set scores...')
         dnn_score = np.average(y_test == y_pred_dnn)
-        if self.collect_knn:
-            knn_score = np.average(y_test == y_pred_knn)
-        if self.collect_svm:
-            svm_score = np.average(y_test == y_pred_svm)
-        if self.collect_lr:
-            lr_score  = np.average(y_test == y_pred_lr)
+        np.place(train_dnn_predictions_prob, train_dnn_predictions_prob == 0.0, [eps])  # for KL divergences
+        np.place(test_dnn_predictions_prob , test_dnn_predictions_prob  == 0.0, [eps])  # for KL divergences
+        self.tb_logger_test.log_scalar('dnn_score', dnn_score, self.global_step)
 
-        self.log.info('Calculate ma/md and psame scores...')
-        if self.collect_knn:
-            ma_score_knn, md_score_knn = calc_mutual_agreement(y_pred_dnn, y_pred_knn, y_test)
-        if self.collect_svm:
-            ma_score_svm, md_score_svm = calc_mutual_agreement(y_pred_dnn, y_pred_svm, y_test)
-        if self.collect_lr:
-            ma_score_lr , md_score_lr  = calc_mutual_agreement(y_pred_dnn, y_pred_lr , y_test)
-        if self.collect_knn:
-            psame_knn = calc_psame(y_pred_dnn, y_pred_knn)
-        if self.collect_svm:
-            psame_svm = calc_psame(y_pred_dnn, y_pred_svm)
-        if self.collect_lr:
-            psame_lr  = calc_psame(y_pred_dnn, y_pred_lr)
-
-        self.log.info('Calculate KL divergences...')
-        np.place(test_dnn_predictions_prob, test_dnn_predictions_prob == 0.0, [eps])
-        if self.collect_knn:
-            np.place(test_knn_predictions_prob, test_knn_predictions_prob == 0.0, [eps])
-            dnn_knn_kl_div = entropy(test_dnn_predictions_prob, test_knn_predictions_prob)
-            dnn_knn_kl_div_avg = np.average(dnn_knn_kl_div)
-        if self.collect_lr:
-            np.place(test_lr_predictions_prob, test_lr_predictions_prob == 0.0, [eps])
-            dnn_lr_kl_div  = entropy(test_dnn_predictions_prob, test_lr_predictions_prob)
-            dnn_lr_kl_div_avg  = np.average(dnn_lr_kl_div)
+        for model_name in ['knn', 'svm', 'lr']:
+            if not (model_name is 'knn' and self.collect_knn) or \
+                   (model_name is 'svm' and self.collect_svm) or \
+                   (model_name is 'lr'  and self.collect_lr):
+                continue
+            self.process(model_name=model_name,
+                         dataset_name='test',
+                         X=X_test_features,
+                         y=y_test,
+                         dnn_predictions_prob=test_dnn_predictions_prob)
 
         if self.eval_trainset:
-            # special fitting
             if self.collect_knn:
-                self.log.info('Fitting KNN model...')
+                self.log.info('Fitting KNN model for training set...')
                 self.knn_train.fit(X_train_features, y_train)
-
-            # predictions (train set)
-            self.log.info('Predicting train set labels from DNN model...')
-            y_pred_dnn_train = train_dnn_predictions_prob.argmax(axis=1)
-            if self.collect_knn:
-                self.log.info('Predicting train set labels from KNN model...')
-                train_knn_predictions_prob = self.knn_predict_proba_for_trainset(X_train_features, y_train)
-                y_pred_knn_train = train_knn_predictions_prob.argmax(axis=1)
-            if self.collect_svm:
-                self.log.info('Predicting train set labels from SVM model...')
-                y_pred_svm_train = self.svm.predict(X_train_features)
-            if self.collect_lr:
-                self.log.info('Predicting train set labels from Logistic Regression model...')
-                train_lr_predictions_prob = self.lr.predict_proba(X_train_features)
-                y_pred_lr_train = train_lr_predictions_prob.argmax(axis=1)
-
-            # calculate metrics
-            self.log.info('Calculate train set scores...')
-            dnn_score_train = np.average(y_train == y_pred_dnn_train)
-            if self.collect_knn:
-                knn_score_train = np.average(y_train == y_pred_knn_train)
-            if self.collect_svm:
-                svm_score_train = np.average(y_train == y_pred_svm_train)
-            if self.collect_lr:
-                lr_score_train  = np.average(y_train == y_pred_lr_train)
-            self.log.info('Calculate ma/md and psame scores...')
-            if self.collect_knn:
-                ma_score_knn_train, md_score_knn_train = calc_mutual_agreement(y_pred_dnn_train, y_pred_knn_train, y_train)
-            if self.collect_svm:
-                ma_score_svm_train, md_score_svm_train = calc_mutual_agreement(y_pred_dnn_train, y_pred_svm_train, y_train)
-            if self.collect_lr:
-                ma_score_lr_train , md_score_lr_train  = calc_mutual_agreement(y_pred_dnn_train, y_pred_lr_train , y_train)
-            if self.collect_knn:
-                psame_knn_train = calc_psame(y_pred_dnn_train, y_pred_knn_train)
-            if self.collect_svm:
-                psame_svm_train = calc_psame(y_pred_dnn_train, y_pred_svm_train)
-            if self.collect_lr:
-                psame_lr_train  = calc_psame(y_pred_dnn_train, y_pred_lr_train)
-            self.log.info('Calculate KL divergences...')
-            np.place(train_dnn_predictions_prob, train_dnn_predictions_prob == 0.0, [eps])
-            if self.collect_knn:
-                np.place(train_knn_predictions_prob, train_knn_predictions_prob == 0.0, [eps])
-                dnn_knn_kl_div_train = entropy(train_dnn_predictions_prob, train_knn_predictions_prob)
-                dnn_knn_kl_div_avg_train = np.average(dnn_knn_kl_div_train)
-            if self.collect_lr:
-                np.place(train_lr_predictions_prob, train_lr_predictions_prob == 0.0, [eps])
-                dnn_lr_kl_div_train  = entropy(train_dnn_predictions_prob, train_lr_predictions_prob)
-                dnn_lr_kl_div_avg_train = np.average(dnn_lr_kl_div_train)
+            for model_name in ['knn', 'svm', 'lr']:
+                if not (model_name is 'knn' and self.collect_knn) or \
+                       (model_name is 'svm' and self.collect_svm) or \
+                       (model_name is 'lr'  and self.collect_lr):
+                    continue
+                self.process(model_name=model_name,
+                             dataset_name='train',
+                             X=X_train_features,
+                             y=y_train,
+                             dnn_predictions_prob=train_dnn_predictions_prob)
 
         self.test_retention.add_score(dnn_score, self.global_step)
-
-        # save summaries
-        self.tb_logger_test.log_scalar('dnn_score', dnn_score, self.global_step)
-        if self.collect_knn:
-            self.tb_logger_test.log_scalar('knn_score', knn_score, self.global_step)
-        if self.collect_svm:
-            self.tb_logger_test.log_scalar('svm_score', svm_score, self.global_step)
-        if self.collect_lr:
-            self.tb_logger_test.log_scalar('lr_score' , lr_score , self.global_step)
-
-        if self.collect_knn:
-            self.tb_logger_test.log_scalar('knn_ma_score', ma_score_knn, self.global_step)
-            self.tb_logger_test.log_scalar('knn_md_score', md_score_knn, self.global_step)
-        if self.collect_svm:
-            self.tb_logger_test.log_scalar('svm_ma_score', ma_score_svm, self.global_step)
-            self.tb_logger_test.log_scalar('svm_md_score', md_score_svm, self.global_step)
-        if self.collect_lr:
-            self.tb_logger_test.log_scalar('lr_ma_score' , ma_score_lr , self.global_step)
-            self.tb_logger_test.log_scalar('lr_md_score' , md_score_lr , self.global_step)
-        if self.collect_knn:
-            self.tb_logger_test.log_scalar('knn_psame'   , psame_knn   , self.global_step)
-        if self.collect_svm:
-            self.tb_logger_test.log_scalar('svm_psame'   , psame_svm   , self.global_step)
-        if self.collect_lr:
-            self.tb_logger_test.log_scalar('lr_psame'    , psame_lr   , self.global_step)
-
-        if self.collect_knn:
-            self.tb_logger_test.log_scalar('dnn_knn_kl_div_avg', dnn_knn_kl_div_avg, self.global_step)
-        if self.collect_lr:
-            self.tb_logger_test.log_scalar('dnn_lr_kl_div_avg' , dnn_lr_kl_div_avg , self.global_step)
-
-        if self.eval_trainset:
-            self.tb_logger_test.log_scalar('dnn_score_trainset', dnn_score_train, self.global_step)
-            if self.collect_knn:
-                self.tb_logger_test.log_scalar('knn_score_trainset', knn_score_train, self.global_step)
-            if self.collect_svm:
-                self.tb_logger_test.log_scalar('svm_score_trainset', svm_score_train, self.global_step)
-            if self.collect_lr:
-                self.tb_logger_test.log_scalar('lr_score_trainset' , lr_score_train, self.global_step)
-
-            if self.collect_knn:
-                self.tb_logger_test.log_scalar('knn_ma_score_trainset', ma_score_knn_train, self.global_step)
-                self.tb_logger_test.log_scalar('knn_md_score_trainset', md_score_knn_train, self.global_step)
-            if self.collect_svm:
-                self.tb_logger_test.log_scalar('svm_ma_score_trainset', ma_score_svm_train, self.global_step)
-                self.tb_logger_test.log_scalar('svm_md_score_trainset', md_score_svm_train, self.global_step)
-            if self.collect_lr:
-                self.tb_logger_test.log_scalar('lr_ma_score_trainset' , ma_score_lr_train, self.global_step)
-                self.tb_logger_test.log_scalar('lr_md_score_trainset' , md_score_lr_train, self.global_step)
-            if self.collect_knn:
-                self.tb_logger_test.log_scalar('knn_psame_trainset', psame_knn_train, self.global_step)
-            if self.collect_svm:
-                self.tb_logger_test.log_scalar('svm_psame_trainset', psame_svm_train, self.global_step)
-            if self.collect_lr:
-                self.tb_logger_test.log_scalar('lr_psame_trainset' , psame_lr_train, self.global_step)
-
-            if self.collect_knn:
-                self.tb_logger_test.log_scalar('dnn_knn_kl_div_avg_trainset', dnn_knn_kl_div_avg_train, self.global_step)
-            if self.collect_lr:
-                self.tb_logger_test.log_scalar('dnn_lr_kl_div_avg_trainset' , dnn_lr_kl_div_avg_train , self.global_step)
-
         self.summary_writer_test.flush()
 
     def to_test(self):
