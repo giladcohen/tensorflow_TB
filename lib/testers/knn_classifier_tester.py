4206a82ac2d7ebcc0f0f7cfafdb5680940540691
giladcohen <giladadlig@gmail.com>
2018-09-22 16:47:50 +0300

    no message

--- lib/testers/knn_classifier_tester.py
+++ lib/testers/knn_classifier_tester.py
@@ -60,7 +60,7 @@ class KNNClassifierTester(TesterBase):
             kernel='linear',
             probability=True,
             random_state=self.rand_gen,
-            tol=20.0
+            tol=10.0
         )
 
         self.lr = LogisticRegression(
