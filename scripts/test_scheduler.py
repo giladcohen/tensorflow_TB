880a2c2d8ac9357b75fbb6f136f9cf9cf38495b1
Gilad Cohen <giladdalig@gmail.com>
2018-12-16 13:07:23 +0200

    modified:   scripts/test_scheduler.py

--- scripts/test_scheduler.py
+++ scripts/test_scheduler.py
@@ -12,11 +12,11 @@ logdir_vec = []
 for train_size in range(1, 61):
     logdir_vec.append('/data/gilad/logs/knn_bayes/wrn/mnist/log_bs_200_lr_0.1s_n_{}k-SUPERSEED=23111800'.format(train_size))
 
-knn_norm = 'L1'
+knn_norm = 'L2'
 
 for logdir in logdir_vec:
     train_validation_info = os.path.join(logdir, 'train_validation_info.csv')
-    cmd = 'CUDA_VISIBLE_DEVICES=2 python scripts/test_automated.py' + \
+    cmd = 'CUDA_VISIBLE_DEVICES=3 python scripts/test_automated.py' + \
           ' --ROOT_DIR ' + logdir + \
           ' --KNN_NORM ' + knn_norm + \
           ' --CHECKPOINT_FILE ' + 'model_schedule.ckpt-3000' \
