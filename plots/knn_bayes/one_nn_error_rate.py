e6f517fca3e8a40485f86404f0108bbfb9c6c748
giladcohen <giladadlig@gmail.com>
2018-12-19 17:06:53 +0200

    no message

--- plots/knn_bayes/one_nn_error_rate.py
+++ plots/knn_bayes/one_nn_error_rate.py
@@ -1,13 +1,22 @@
-import subprocess
-import time
+"""In this script I print:
+the 1-NN error rate as a function of the train samples
+ +
+the 1-NN error rate - DNN error rate as a function of the train samples
+ """
+
+from utils.plots import load_data_from_csv_wrapper, add_subplot_axes
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
 import os
+import json
 
-def run_cmd(cmd):
-    print ('start running command:\n{}'.format(cmd))
-    process = subprocess.call(cmd, shell=True)
-    print ('finished running command: {}'.format(cmd))
+plt.rcParams['interactive'] = False
+subpos = np.array([0.35, 0.25, 0.5, 0.4])
+fig = plt.figure(figsize=(15.0, 8.0))
 
-
+measure = 'knn_k_3_norm_L1_knn_score'
+# setting all experiments
 logdir_vec = [
     '/data/gilad/logs/knn_bayes/wrn/mnist/log_bs_30_lr_0.1s_n_0.03k-SUPERSEED=23111800',
     '/data/gilad/logs/knn_bayes/wrn/mnist/log_bs_40_lr_0.1s_n_0.04k-SUPERSEED=23111800',
@@ -26,26 +35,24 @@ logdir_vec = [
     '/data/gilad/logs/knn_bayes/wrn/mnist/log_bs_200_lr_0.1s_n_0.8k-SUPERSEED=23111800',
     '/data/gilad/logs/knn_bayes/wrn/mnist/log_bs_200_lr_0.1s_n_0.9k-SUPERSEED=23111800',
 ]
-
+n_vec = [30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900]
 for i in range(1, 61):
     logdir_vec.append('/data/gilad/logs/knn_bayes/wrn/mnist/log_bs_200_lr_0.1s_n_{}k-SUPERSEED=23111800'.format(i))
+    n_vec.append(int(i * 1000))
 
-# create all dictionaries
-for logdir in logdir_vec:
-    if not os.path.exists(logdir):
-        os.makedirs(logdir)
-    cmd = 'scp -r gilad@132.66.196.128:' + logdir + '/data_for_figures ' + logdir + '/data_for_figures'
-    run_cmd(cmd)
+knn_error_rate = []
+for root_dir in logdir_vec:
+    json_file = os.path.join(root_dir, 'data_for_figures', 'data.json')
+    with open(json_file) as f:
+        data = json.load(f)
+    knn_error_rate.append(1.0 - data['test']['regular'][measure]['values'][0])
 
-print('end of script.')
+ax1 = fig.add_subplot(111)
+ax1.semilogx(n_vec, knn_error_rate)
+ax1.yaxis.grid()
+ax1.set_ylabel('3-NN error rate', labelpad=5, fontdict={'fontsize': 12})
+ax1.set_xlabel('number of samples')
+ax1.set_title('KNN error rate (K=3)')
 
-
-
-
-
-
-
-
-
-
-
+plt.tight_layout()
+plt.savefig('three_nn_error_rate.png')
