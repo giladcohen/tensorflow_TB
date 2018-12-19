8286157f2864c1d4269327c9a4427d339d458ed8
giladcohen <giladadlig@gmail.com>
2018-12-19 18:10:45 +0200

    no message

--- examples/train/train_cifar10_bayes_scheduler.py
+++ examples/train/train_cifar10_bayes_scheduler.py
@@ -8,13 +8,15 @@ def run_cmd(cmd):
     print ('finished running command {}'.format(cmd))
     time.sleep(3)
 
-n_vec = np.arange(1, 18)
+# n_vec = np.arange(1, 18)
+n_vec = np.arange(0.2, 1, 0.1)
+
 for n in n_vec:
     logdir = '/data/gilad/logs/knn_bayes/wrn/cifar10/log_bs_200_lr_0.1s_n_{}k-SUPERSEED=19121800'.format(n)
-    cmd = 'CUDA_VISIBLE_DEVICES=0 python scripts/train_automated.py' + \
+    cmd = 'CUDA_VISIBLE_DEVICES=3 python scripts/train_automated.py' + \
           ' --ROOT_DIR ' + logdir + \
           ' --SUPERSEED ' + logdir[-8:] + \
-          ' --TRAIN_SET_SIZE ' + str(n)+'000' + \
+          ' --TRAIN_SET_SIZE ' + str(int(n * 1000)) + \
           ' --ARCHITECTURE ' + 'Wide-Resnet-28-10' \
           ' -c examples/train/train_simple_cifar10.ini'
     run_cmd(cmd)
