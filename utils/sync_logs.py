8f45f31bc46cbd2f4e944d397c068d7106e471e8
giladcohen <giladadlig@gmail.com>
2019-01-13 22:19:38 +0200

    no message

--- utils/sync_logs.py
+++ utils/sync_logs.py
@@ -13,15 +13,7 @@ def run_cmd(cmd):
 #     logdir_vec.append('/data/gilad/logs/knn_bayes/wrn/mnist/w_dropout/log_bs_200_lr_0.1s_n_{}k-SUPERSEED=30121800'.format(i))
 
 logdir_vec = [
-    '/data/gilad/logs/knn_bayes/wrn/cifar10_cats_v_dogs/w_dropout/log_bs_200_lr_0.1s_n_1k-SUPERSEED=08011900',
-    '/data/gilad/logs/knn_bayes/wrn/cifar10_cats_v_dogs/w_dropout/log_bs_200_lr_0.1s_n_2k-SUPERSEED=08011900',
-    '/data/gilad/logs/knn_bayes/wrn/cifar10_cats_v_dogs/w_dropout/log_bs_200_lr_0.1s_n_3k-SUPERSEED=08011900',
-    '/data/gilad/logs/knn_bayes/wrn/cifar10_cats_v_dogs/w_dropout/log_bs_200_lr_0.1s_n_4k-SUPERSEED=08011900',
-    '/data/gilad/logs/knn_bayes/wrn/cifar10_cats_v_dogs/w_dropout/log_bs_200_lr_0.1s_n_5k-SUPERSEED=08011900',
-    '/data/gilad/logs/knn_bayes/wrn/cifar10_cats_v_dogs/w_dropout/log_bs_200_lr_0.1s_n_6k-SUPERSEED=08011900',
-    '/data/gilad/logs/knn_bayes/wrn/cifar10_cats_v_dogs/w_dropout/log_bs_200_lr_0.1s_n_7k-SUPERSEED=08011900',
-    '/data/gilad/logs/knn_bayes/wrn/cifar10_cats_v_dogs/w_dropout/log_bs_200_lr_0.1s_n_8k-SUPERSEED=08011900',
-    '/data/gilad/logs/knn_bayes/wrn/cifar10_cats_v_dogs/w_dropout/log_bs_200_lr_0.1s_n_9k-SUPERSEED=08011900',
+    '/data/gilad/logs/knn_bayes/wrn/cifar10_cats_v_dogs/w_dropout/log_bs_200_lr_0.1s_n_10k-SUPERSEED=08011900',
 ]
 
 # create all dictionaries
@@ -30,7 +22,9 @@ for logdir in logdir_vec:
     if not os.path.exists(data_path):
         os.makedirs(data_path)
     # cmd = 'scp -r gilad@132.66.196.128:' + logdir + '/data_for_figures ' + logdir + '/data_for_figures' # takes too long
-    cmd = 'scp -r gilad@132.66.196.128:' + logdir + '/data_for_figures/data.json ' + logdir + '/data_for_figures/data.json'
+    # cmd = 'scp -r gilad@132.66.196.128:' + logdir + '/data_for_figures/data.json ' + logdir + '/data_for_figures/data.json'
+    cmd = 'scp -r giladdalig@35.233.9.152:' + logdir + '/data_for_figures/data.json ' + logdir + '/data_for_figures/data.json'
+
     run_cmd(cmd)
 
 print('end of script.')
