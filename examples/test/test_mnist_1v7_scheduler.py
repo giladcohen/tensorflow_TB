b37c60653b2950783e88610232d26a0afa7bf0de
giladcohen <giladadlig@gmail.com>
2019-01-14 17:20:44 +0200

    no message

--- examples/test/test_mnist_1v7_scheduler.py
+++ examples/test/test_mnist_1v7_scheduler.py
@@ -10,14 +10,23 @@ def run_cmd(cmd):
     time.sleep(3)
 
 knn_norm = 'L1'
-n_vec = np.arange(1, 7)
+n_vec = np.arange(1, 4)
 
 for n in n_vec:
     logdir = '/data/gilad/logs/knn_bayes/wrn/mnist_1v7/w_dropout/log_bs_200_lr_0.1s_n_{}k-SUPERSEED=08011900'.format(n)
     train_validation_info = os.path.join(logdir, 'train_validation_info.csv')
     cmd = 'CUDA_VISIBLE_DEVICES=0 python scripts/test_automated.py' + \
           ' --ROOT_DIR ' + logdir + \
-          ' --KNN_NORM ' + knn_norm + \
+          ' --KNN_NORM ' + 'L1' + \
+          ' --PCA_REDUCTION False' + \
+          ' --CHECKPOINT_FILE ' + 'model_schedule.ckpt-3000' + \
+          ' --TRAIN_VALIDATION_MAP_REF ' + train_validation_info + \
+          ' --DROPOUT_KEEP_PROB 1.0' + \
+          ' -c examples/test/test_multi_knn.ini'
+    run_cmd(cmd)
+    cmd = 'CUDA_VISIBLE_DEVICES=0 python scripts/test_automated.py' + \
+          ' --ROOT_DIR ' + logdir + \
+          ' --KNN_NORM ' + 'L2' + \
           ' --PCA_REDUCTION False' + \
           ' --CHECKPOINT_FILE ' + 'model_schedule.ckpt-3000' + \
           ' --TRAIN_VALIDATION_MAP_REF ' + train_validation_info + \
@@ -25,17 +34,18 @@ for n in n_vec:
           ' -c examples/test/test_multi_knn.ini'
     run_cmd(cmd)
 
-for n in n_vec:
-    logdir = '/data/gilad/logs/knn_bayes/wrn/mnist_1v7/w_dropout/log_bs_200_lr_0.1s_n_{}k-SUPERSEED=08011900'.format(n)
-    train_validation_info = os.path.join(logdir, 'train_validation_info.csv')
-    cmd = 'CUDA_VISIBLE_DEVICES=0 python scripts/test_automated.py' + \
-          ' --ROOT_DIR ' + logdir + \
-          ' --KNN_NORM ' + knn_norm + \
-          ' --PCA_REDUCTION False' + \
-          ' --CHECKPOINT_FILE ' + 'model_schedule.ckpt-3000' + \
-          ' --TRAIN_VALIDATION_MAP_REF ' + train_validation_info + \
-          ' --DROPOUT_KEEP_PROB 0.5' + \
-          ' -c examples/test/test_bayesian_multi_knn.ini'
-    run_cmd(cmd)
+
+# for n in n_vec:
+#     logdir = '/data/gilad/logs/knn_bayes/wrn/mnist_1v7/w_dropout/log_bs_200_lr_0.1s_n_{}k-SUPERSEED=08011900'.format(n)
+#     train_validation_info = os.path.join(logdir, 'train_validation_info.csv')
+#     cmd = 'CUDA_VISIBLE_DEVICES=0 python scripts/test_automated.py' + \
+#           ' --ROOT_DIR ' + logdir + \
+#           ' --KNN_NORM ' + knn_norm + \
+#           ' --PCA_REDUCTION False' + \
+#           ' --CHECKPOINT_FILE ' + 'model_schedule.ckpt-3000' + \
+#           ' --TRAIN_VALIDATION_MAP_REF ' + train_validation_info + \
+#           ' --DROPOUT_KEEP_PROB 0.5' + \
+#           ' -c examples/test/test_bayesian_multi_knn.ini'
+#     run_cmd(cmd)
 
 print('end of script.')
