d74612d39e8f2d902c919453253e0d5b19c24b30
Gilad Cohen <giladdalig@gmail.com>
2019-01-17 11:13:55 +0200

    modified:   plots/knn_bayes/cifar10_cats_v_dogs_w_dropout/calc_lipschits_constant.py

--- plots/knn_bayes/cifar10_cats_v_dogs_w_dropout/calc_lipschits_constant.py
+++ plots/knn_bayes/cifar10_cats_v_dogs_w_dropout/calc_lipschits_constant.py
@@ -14,7 +14,7 @@ norm = 'L2'
 def calc_D_vec(sf_vec):
     return sf_vec[:, 1] - sf_vec[:, 0]
 
-n = 1
+n = 10
 logdir = '/data/gilad/logs/knn_bayes/wrn/cifar10_cats_v_dogs/w_dropout/log_bs_200_lr_0.1s_n_{}k-SUPERSEED=08011900'.format(n)
 test_dir = os.path.join(logdir, 'test')
 train_features_file             = os.path.join(test_dir, 'train_features.npy')
@@ -60,10 +60,15 @@ for i in range(0, test_size):
         cnt += 1
 assert cnt == ((test_size - 1)*test_size / 2)
 all_feature_distances = np.array(all_feature_distances)
+all_feature_distances.sort()
 
-num_bins = int(cnt/100)  # I want just one percent of the distances
-dist_hist, bin_edges = np.histogram(all_feature_distances, num_bins)
-max_embedded_dist = bin_edges[1]
+# I want to take just the 0.05% of the distances. therefore...
+index = all_feature_distances.shape[0] // 200
+max_embedded_dist = all_feature_distances[index]
+
+# num_bins = int(cnt/100)  # I want just one percent of the distances
+# dist_hist, bin_edges = np.histogram(all_feature_distances, num_bins)
+# max_embedded_dist = bin_edges[1]
 
 # for every ||x-y|| < max_embedded_dist, we need to find |D(x)-D(z)|
 all_feature_distances = []
@@ -78,7 +83,7 @@ for i in range(0, test_size):
 all_feature_distances = np.array(all_feature_distances)
 all_D_distances       = np.array(all_D_distances)
 D_div_xz              = all_D_distances/all_feature_distances
-plt.scatter(all_feature_distances, D_div_xz)
+plt.scatter(all_feature_distances, D_div_xz, s=0.5)
 plt.xlabel('||x-z||')
 plt.ylabel('|D(x)-D(z)|/||x-z||')
 C_Lipschits = np.max(D_div_xz)
