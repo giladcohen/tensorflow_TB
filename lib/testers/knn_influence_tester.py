0da7f7fa57e23989e647f533e36fa5258589881c
giladcohen <giladadlig@gmail.com>
2019-02-27 08:33:52 +0200

    no message

--- lib/testers/knn_influence_tester.py
+++ lib/testers/knn_influence_tester.py
@@ -6,6 +6,7 @@ import numpy as np
 from lib.testers.knn_classifier_tester import KNNClassifierTester
 from sklearn.neighbors import NearestNeighbors
 from lib.datasets.influence_feeder import Feeder
+import darkon
 import matplotlib.pyplot as plt
 
 class KNNInfluenceTester(KNNClassifierTester):
@@ -19,6 +20,19 @@ class KNNInfluenceTester(KNNClassifierTester):
                                     n_jobs=self.knn_jobs)
 
         self.feeder = Feeder('influence_feeder', self.prm)
+
+        self._classes = (
+            'airplane',
+            'automobile',
+            'bird',
+            'cat',
+            'deer',
+            'dog',
+            'frog',
+            'horse',
+            'ship',
+            'truck'
+        )
 
     def test(self):
         # building the feeder
@@ -45,10 +59,72 @@ class KNNInfluenceTester(KNNClassifierTester):
         # find all its nearest neighbors
         self.knn.fit(X_train_features)
         neighbors_indices, neighbors_dists = self.knn.kneighbors(np.expand_dims(test_features, axis=0))
+        print('neighbors_indices={}'.format(neighbors_indices))
 
-        
+        # now find the influence
+        inspector = darkon.Influence(
+            workspace='./influence_workspace',
+            feeder=self.feeder,
+            loss_op_train=self.model.cost,
+            loss_op_test=self.model.cost,
+            x_placeholder=self.model.images,
+            y_placeholder=self.model.labels)
 
+        approx_params = {
+            'scale': 200,
+            'num_repeats': 5,
+            'recursion_depth': 100,
+            'recursion_batch_size': 100
+        }
 
+        scores = inspector.upweighting_influence_batch(
+            sess=self.sess,
+            test_indices=[test_index],
+            test_batch_size=self.prm.train.train_control.EVAL_BATCH_SIZE,
+            approx_params=approx_params,
+            train_batch_size=self.prm.train.train_control.TRAIN_BATCH_SIZE,
+            train_iterations=500)
 
+        sorted_indices = np.argsort(scores)
+        harmful = sorted_indices[:50]
+        helpful = sorted_indices[-50:][::-1]
 
+        cnt_harmful_in_knn = 0
+        print('\nHarmful:')
+        for idx in harmful:
+            print('[{}] {}'.format(idx, scores[idx]))
+            if idx in neighbors_indices:
+                cnt_harmful_in_knn += 1
 
+        cnt_helpful_in_knn = 0
+        print('\nHelpful:')
+        for idx in helpful:
+            print('[{}] {}'.format(idx, scores[idx]))
+            if idx in neighbors_indices:
+                cnt_helpful_in_knn += 1
+
+        fig, axes1 = plt.subplots(5, 10, figsize=(15, 5))
+        target_idx = 0
+        for j in range(5):
+            for k in range(10):
+                idx = helpful[target_idx]
+                axes1[j][k].set_axis_off()
+                axes1[j][k].imshow(self.feeder.train_origin_data[idx])
+                label_str = self._classes[int(self.feeder.train_label[idx])]
+                axes1[j][k].set_title('[{}]: {}'.format(idx, label_str))
+
+                target_idx += 1
+
+        fig, axes1 = plt.subplots(5, 10, figsize=(15, 5))
+        target_idx = 0
+        for j in range(5):
+            for k in range(10):
+                idx = harmful[target_idx]
+                axes1[j][k].set_axis_off()
+                axes1[j][k].imshow(self.feeder.train_origin_data[idx])
+                label_str = self._classes[int(self.feeder.train_label[idx])]
+                axes1[j][k].set_title('[{}]: {}'.format(idx, label_str))
+
+                target_idx += 1
+
+        print('done')
