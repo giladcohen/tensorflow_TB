530621b1bc2e9374ba698a3b1db51903e404462c
giladcohen <giladadlig@gmail.com>
2019-02-27 08:11:21 +0200

    no message

--- lib/testers/knn_influence_tester.py
+++ lib/testers/knn_influence_tester.py
@@ -44,8 +44,9 @@ class KNNInfluenceTester(KNNClassifierTester):
 
         # find all its nearest neighbors
         self.knn.fit(X_train_features)
-        neighbors = self.knn.kneighbors(np.expand_dims(test_features, axis=0))
-        print('cool')
+        neighbors_indices, neighbors_dists = self.knn.kneighbors(np.expand_dims(test_features, axis=0))
+
+        
 
 
 
