70ff9f312f8e3f406dfb8c61c889b2d4eb2a921f
giladcohen <giladadlig@gmail.com>
2019-05-05 17:01:46 +0300

    no message

--- scripts/cifar10_adv_evaluate.py
+++ scripts/cifar10_adv_evaluate.py
@@ -32,7 +32,7 @@ flags.DEFINE_float('weight_decay', 0.0004, 'weight decay')
 flags.DEFINE_string('checkpoint_name', 'cifar10/log_080419_b_125_wd_0.0004_mom_lr_0.1_f_0.9_p_3_c_2_val_size_1000', 'checkpoint name')
 flags.DEFINE_float('label_smoothing', 0.1, 'label smoothing')
 flags.DEFINE_string('workspace', 'influence_workspace_test_mini', 'workspace dir')
-flags.DEFINE_bool('prepare', True, 'whether or not we are in the prepare phase, when hvp is calculated')
+flags.DEFINE_bool('prepare', False, 'whether or not we are in the prepare phase, when hvp is calculated')
 flags.DEFINE_string('set', 'test', 'val or test set to evaluate')
 flags.DEFINE_bool('use_train_mini', True, 'Whether or not to use 5000 training samples instead of 49000')
 flags.DEFINE_string('dataset', 'cifar10', 'datasset: cifar10/100')
@@ -344,8 +344,8 @@ approx_params = {
 }
 
 # sub_relevant_indices = [ind for ind in info[FLAGS.set] if info[FLAGS.set][ind]['net_succ'] and info[FLAGS.set][ind]['attack_succ']]
-sub_relevant_indices = [ind for ind in info[FLAGS.set] if not info[FLAGS.set][ind]['net_succ']]
 # sub_relevant_indices = [ind for ind in info[FLAGS.set]]
+sub_relevant_indices = [ind for ind in info[FLAGS.set] if not info[FLAGS.set][ind]['net_succ']]
 relevant_indices     = [info[FLAGS.set][ind]['global_index'] for ind in sub_relevant_indices]
 
 # b, e = 0, 2500
--- scripts/cifar10_adv_val_pred_adv_classification.py
+++ scripts/cifar10_adv_val_pred_adv_classification.py
@@ -12,28 +12,50 @@ import darkon_examples.cifar10_resnet.cifar10_input as cifar10_input
 import matplotlib.pyplot as plt
 from tensorflow_TB.lib.datasets.influence_feeder_val_test import MyFeederValTest
 import pickle
+from sklearn import metrics
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('checkpoint_name', 'log_080419_b_125_wd_0.0004_mom_lr_0.1_f_0.9_p_3_c_2_val_size_1000', 'checkpoint name')
+flags.DEFINE_string('checkpoint_name', 'cifar10/log_080419_b_125_wd_0.0004_mom_lr_0.1_f_0.9_p_3_c_2_val_size_1000', 'checkpoint name')
 flags.DEFINE_string('set', 'val', 'val or test set to evaluate')
 flags.DEFINE_bool('use_train_mini', False, 'Whether or not to use 5000 training samples instead of 49000')
+flags.DEFINE_string('dataset', 'cifar10', 'datasset: cifar10/100')
 
 test_val_set = FLAGS.set == 'val'
 
-# cifar-10 classes
-_classes = (
-    'airplane',
-    'car',
-    'bird',
-    'cat',
-    'deer',
-    'dog',
-    'frog',
-    'horse',
-    'ship',
-    'truck'
-)
+# cifar10 classes
+if FLAGS.dataset == 'cifar10':
+    _classes = (
+        'airplane',
+        'car',
+        'bird',
+        'cat',
+        'deer',
+        'dog',
+        'frog',
+        'horse',
+        'ship',
+        'truck'
+    )
+elif FLAGS.dataset == 'cifar100':
+    _classes = (
+        'apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle',
+        'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel',
+        'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock',
+        'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur',
+        'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster',
+        'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion',
+        'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse',
+        'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear',
+        'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine',
+        'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose',
+        'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake',
+        'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table',
+        'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout',
+        'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm'
+    )
+else:
+    raise AssertionError('dataset {} not supported'.format(FLAGS.dataset))
 
 # Get CIFAR-10 data
 cifar10_input.maybe_download_and_extract()
@@ -47,7 +69,7 @@ model_dir     = os.path.join('/data/gilad/logs/influence', FLAGS.checkpoint_name
 mini_train_inds = None  # np.load(os.path.join(model_dir, 'train_mini_indices.npy'))
 val_indices     = np.load(os.path.join(model_dir, 'val_indices.npy'))
 
-feeder = MyFeederValTest(rand_gen=rand_gen, as_one_hot=True, val_inds=val_indices,
+feeder = MyFeederValTest(dataset=FLAGS.dataset, rand_gen=rand_gen, as_one_hot=True, val_inds=val_indices,
                          test_val_set=False, mini_train_inds=mini_train_inds)
 
 # get the data
@@ -126,12 +148,13 @@ else:
         info_old = pickle.load(handle)
     assert info == info_old
 
-sub_relevant_indices = [ind for ind in info[FLAGS.set]]
+sub_relevant_indices = [ind for ind in info[FLAGS.set] if info[FLAGS.set][ind]['net_succ']]  #TODO(remove after bug fix)
 relevant_indices     = [info[FLAGS.set][ind]['global_index'] for ind in sub_relevant_indices]
 
 # guessing a threshold rank/dist for the "adv"/"non-adversarial" decision
-max_indices        = np.arange(600, 1001, 10)
-rank_values        = np.arange(4600, 5000, 10)
+# max_indices        = np.arange(600, 1001, 10)
+max_indices        = np.array([800])
+rank_values        = np.arange(10, 10000, 10)
 dist_values        = np.arange(3.7, 3.7, 0.01)
 rank_x_dist_values = np.arange(18000, 18000, 100)
 
@@ -143,13 +166,104 @@ def rxd():
     return np.zeros(shape=(max_indices.shape[0], rank_x_dist_values.shape[0]))
 
 
-metrics = {
-    'rank_thd'   : {'tp': rt() , 'fp': rt() , 'tn': rt() , 'fn': rt() , 'precision': rt() , 'recall': rt() , 'accuracy': rt()},
-    'dist_thd'   : {'tp': dt() , 'fp': dt() , 'tn': dt() , 'fn': dt() , 'precision': dt() , 'recall': dt() , 'accuracy': dt()},
-    'rank_x_dist': {'tp': rxd(), 'fp': rxd(), 'tn': rxd(), 'fn': rxd(), 'precision': rxd(), 'recall': rxd(), 'accuracy': rxd()}
-}
+# metrics = {
+#     'rank_thd'   : {'tp': rt() , 'fp': rt() , 'tn': rt() , 'fn': rt() , 'precision': rt() , 'recall': rt() , 'accuracy': rt()},
+#     'dist_thd'   : {'tp': dt() , 'fp': dt() , 'tn': dt() , 'fn': dt() , 'precision': dt() , 'recall': dt() , 'accuracy': dt()},
+#     'rank_x_dist': {'tp': rxd(), 'fp': rxd(), 'tn': rxd(), 'fn': rxd(), 'precision': rxd(), 'recall': rxd(), 'accuracy': rxd()}
+# }
+#
+# This loop ends up collecting metrics for the entire set, each for different max_indices and rank_value
+# for i, sub_index in enumerate(sub_relevant_indices):
+#     global_index = feeder.val_inds[sub_index]
+#     print('start working on sample i={}. global_index={}'.format(i, global_index))
+#     assert global_index == relevant_indices[i]
+#
+#     real_label = y_val_sparse[sub_index]
+#     pred_label = x_val_preds[sub_index]
+#     adv_label  = x_val_preds_adv[sub_index]
+#
+#     net_succ    = info[FLAGS.set][sub_index]['net_succ']
+#     attack_succ = info[FLAGS.set][sub_index]['attack_succ']
+#
+#     if attack_succ:
+#         assert pred_label != adv_label, 'failed for i={}, sub_index={}, global_index={}'.format(i, sub_index, global_index)
+#     if net_succ:
+#         assert pred_label == real_label, 'failed for i={}, sub_index={}, global_index={}'.format(i, sub_index, global_index)
+#
+#     index_dir = os.path.join(model_dir, 'val', 'val_index_{}'.format(global_index))
+#
+#     # collect pred (negative)
+#     scores_dir = 'real' if net_succ else 'pred'
+#     pred_helpful_ranks = np.load(os.path.join(index_dir, scores_dir, 'helpful_ranks.npy'))
+#     pred_helpful_dists = np.load(os.path.join(index_dir, scores_dir, 'helpful_dists.npy'))
+#     pred_harmful_ranks = np.load(os.path.join(index_dir, scores_dir, 'harmful_ranks.npy'))
+#     pred_harmful_dists = np.load(os.path.join(index_dir, scores_dir, 'harmful_dists.npy'))
+#
+#     if attack_succ:
+#         scores_dir = 'adv'
+#         adv_helpful_ranks = np.load(os.path.join(index_dir, scores_dir, 'helpful_ranks.npy'))
+#         adv_helpful_dists = np.load(os.path.join(index_dir, scores_dir, 'helpful_dists.npy'))
+#         adv_harmful_ranks = np.load(os.path.join(index_dir, scores_dir, 'harmful_ranks.npy'))
+#         adv_harmful_dists = np.load(os.path.join(index_dir, scores_dir, 'harmful_dists.npy'))
+#
+#     for m, max_index in enumerate(max_indices):
+#         # rank_thd
+#         for r, rank_thd in enumerate(rank_values):
+#             # calculate rank_thd for negative pred
+#             ranks_mean = pred_helpful_ranks[0:max_index].mean()
+#             if ranks_mean < rank_thd:
+#                 metrics['rank_thd']['tn'][m, r] += 1
+#             else:
+#                 metrics['rank_thd']['fn'][m, r] += 1
+#
+#             if attack_succ:
+#                 # calculate rank_thd for positive adv
+#                 ranks_mean = adv_helpful_ranks[0:max_index].mean()
+#                 if ranks_mean >= rank_thd:
+#                     metrics['rank_thd']['tp'][m, r] += 1
+#                 else:
+#                     metrics['rank_thd']['fp'][m, r] += 1
+#
+#         # dist_thd
+#         for d, dist_thd in enumerate(dist_values):
+#             # calculate dist_thd for negative pred
+#             dists_mean = pred_helpful_dists[0:max_index].mean()
+#             if dists_mean < dist_thd:
+#                 metrics['dist_thd']['tn'][m, d] += 1
+#             else:
+#                 metrics['dist_thd']['fn'][m, d] += 1
+#
+#             if attack_succ:
+#                 # calculate dist_thd for positive adv
+#                 dists_mean = adv_helpful_dists[0:max_index].mean()
+#                 if dists_mean >= dist_thd:
+#                     metrics['dist_thd']['tp'][m, d] += 1
+#                 else:
+#                     metrics['dist_thd']['fp'][m, d] += 1
+#
+#         # rank_x_dist
+#         for rd, rank_x_dist_thd in enumerate(rank_x_dist_values):
+#             # calculate rank_x_dist_thd for negative pred
+#             rxd_mean = (pred_helpful_ranks[0:max_index] * pred_helpful_dists[0:max_index]).mean()
+#             if rxd_mean < rank_x_dist_thd:
+#                 metrics['rank_x_dist']['tn'][m, rd] += 1
+#             else:
+#                 metrics['rank_x_dist']['fn'][m, rd] += 1
+#
+#             if attack_succ:
+#                 # calculate rank_x_dist_thd for positive adv
+#                 rxd_mean = (adv_helpful_ranks[0:max_index] * adv_helpful_dists[0:max_index]).mean()
+#                 if rxd_mean >= rank_x_dist_thd:
+#                     metrics['rank_x_dist']['tp'][m, rd] += 1
+#                 else:
+#                     metrics['rank_x_dist']['fp'][m, rd] += 1
+#
+#     del pred_helpful_ranks, pred_helpful_dists, pred_harmful_ranks, pred_harmful_dists
+#     if attack_succ:
+#         del adv_helpful_ranks, adv_helpful_dists, adv_harmful_ranks, adv_harmful_dists
 
-# tp, fp, tn, fn = 0, 0, 0, 0
+y_true  = []
+y_score = []
 for i, sub_index in enumerate(sub_relevant_indices):
     global_index = feeder.val_inds[sub_index]
     print('start working on sample i={}. global_index={}'.format(i, global_index))
@@ -160,6 +274,7 @@ for i, sub_index in enumerate(sub_relevant_indices):
     adv_label  = x_val_preds_adv[sub_index]
 
     net_succ    = info[FLAGS.set][sub_index]['net_succ']
+    assert net_succ  #TODO(remove after bug fix)
     attack_succ = info[FLAGS.set][sub_index]['attack_succ']
 
     if attack_succ:
@@ -183,61 +298,35 @@ for i, sub_index in enumerate(sub_relevant_indices):
         adv_harmful_ranks = np.load(os.path.join(index_dir, scores_dir, 'harmful_ranks.npy'))
         adv_harmful_dists = np.load(os.path.join(index_dir, scores_dir, 'harmful_dists.npy'))
 
-    for m, max_index in enumerate(max_indices):
-        # rank_thd
-        for r, rank_thd in enumerate(rank_values):
-            # calculate rank_thd for negative pred
-            ranks_mean = pred_helpful_ranks[0:max_index].mean()
-            if ranks_mean < rank_thd:
-                metrics['rank_thd']['tn'][m, r] += 1
-            else:
-                metrics['rank_thd']['fn'][m, r] += 1
+    # start with the real label
+    y_true.append(0)
+    ranks_mean = pred_helpful_ranks[0:800].mean()
+    y_score.append(ranks_mean / 50000)
 
-            if attack_succ:
-                # calculate rank_thd for positive adv
-                ranks_mean = adv_helpful_ranks[0:max_index].mean()
-                if ranks_mean >= rank_thd:
-                    metrics['rank_thd']['tp'][m, r] += 1
-                else:
-                    metrics['rank_thd']['fp'][m, r] += 1
-
-        # dist_thd
-        for d, dist_thd in enumerate(dist_values):
-            # calculate dist_thd for negative pred
-            dists_mean = pred_helpful_dists[0:max_index].mean()
-            if dists_mean < dist_thd:
-                metrics['dist_thd']['tn'][m, d] += 1
-            else:
-                metrics['dist_thd']['fn'][m, d] += 1
-
-            if attack_succ:
-                # calculate dist_thd for positive adv
-                dists_mean = adv_helpful_dists[0:max_index].mean()
-                if dists_mean >= dist_thd:
-                    metrics['dist_thd']['tp'][m, d] += 1
-                else:
-                    metrics['dist_thd']['fp'][m, d] += 1
-
-        # rank_x_dist
-        for rd, rank_x_dist_thd in enumerate(rank_x_dist_values):
-            # calculate rank_x_dist_thd for negative pred
-            rxd_mean = (pred_helpful_ranks[0:max_index] * pred_helpful_dists[0:max_index]).mean()
-            if rxd_mean < rank_x_dist_thd:
-                metrics['rank_x_dist']['tn'][m, rd] += 1
-            else:
-                metrics['rank_x_dist']['fn'][m, rd] += 1
-
-            if attack_succ:
-                # calculate rank_x_dist_thd for positive adv
-                rxd_mean = (adv_helpful_ranks[0:max_index] * adv_helpful_dists[0:max_index]).mean()
-                if rxd_mean >= rank_x_dist_thd:
-                    metrics['rank_x_dist']['tp'][m, rd] += 1
-                else:
-                    metrics['rank_x_dist']['fp'][m, rd] += 1
-
-    del pred_helpful_ranks, pred_helpful_dists, pred_harmful_ranks, pred_harmful_dists
+    # add an adv label if attack succeeded
     if attack_succ:
-        del adv_helpful_ranks, adv_helpful_dists, adv_harmful_ranks, adv_harmful_dists
+        y_true.append(1)
+        ranks_mean = adv_helpful_ranks[0:800].mean()
+        y_score.append(ranks_mean / 50000)
+
+y_true  = np.asarray(y_true)
+y_score = np.asarray(y_score)
+fpr, tpr, thresholds = metrics.roc_curve(y_true, y_score)
+auc = metrics.roc_auc_score(y_true, y_score)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 for key in metrics.keys():
     if key == 'rank_thd':
@@ -253,8 +342,8 @@ for key in metrics.keys():
             tp, fp, tn, fn = metrics[key]['tp'][m, r], metrics[key]['fp'][m, r], \
                              metrics[key]['tn'][m, r], metrics[key]['fn'][m, r]
             metrics[key]['precision'][m, r] = tp/(tp+fp)
-            metrics[key]['recall'][m, r]    = tp/(tp+fn)
-            metrics[key]['recall'][m, r]    = tp/(tp+fn)
+            metrics[key]['recall'][m, r]    = tp/(tp+fn)  # = TPR
+            metrics[key]['fpr'][m, r]       = fp/(tn+fp)  # = FPR
             metrics[key]['accuracy'][m, r]  = (tp+tn)/(tp+tn+fp+fn)
 
 # plotting accuracy metric for all methods
