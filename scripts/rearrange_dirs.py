89ad382f697b71c95ab7eaf74afbeb2d28b0b7a6
giladcohen <giladadlig@gmail.com>
2019-05-18 12:55:39 +0300

    no message

--- scripts/rearrange_dirs.py
+++ scripts/rearrange_dirs.py
@@ -7,7 +7,7 @@ def run_cmd(cmd):
     process = subprocess.call(cmd, shell=True)
     print ('finished running command {}'.format(cmd))
 
-dump_file = '/data/gilad/logs/influence/cifar100/log_300419_b_125_wd_0.0004_mom_lr_0.1_f_0.9_p_3_c_2_val_size_1000_ls_0.01/val/dump.txt'
+dump_file = '/data/gilad/logs/influence/cifar100/log_300419_b_125_wd_0.0004_mom_lr_0.1_f_0.9_p_3_c_2_val_size_1000_ls_0.01/test/dump.txt'
 with open(dump_file) as f:
     content = f.readlines()
 # # you may also want to remove whitespace characters like `\n` at the end of each line
@@ -16,7 +16,7 @@ del content[0]
 
 dir_list = []
 for c in content:
-    dir_list.append(os.path.join('/data/gilad/logs/influence/cifar100/log_300419_b_125_wd_0.0004_mom_lr_0.1_f_0.9_p_3_c_2_val_size_1000_ls_0.01/val', c))
+    dir_list.append(os.path.join('/data/gilad/logs/influence/cifar100/log_300419_b_125_wd_0.0004_mom_lr_0.1_f_0.9_p_3_c_2_val_size_1000_ls_0.01/test', c))
 
 for dir in dir_list:
     cmd = 'mkdir -p ' + os.path.join(dir, 'adv', 'deepfool')
