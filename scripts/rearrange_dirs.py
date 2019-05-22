98e93bb2f6cd903df6fb56522d22deec6d2f6866
giladcohen <giladadlig@gmail.com>
2019-05-23 02:01:44 +0300

    no message

--- scripts/rearrange_dirs.py
+++ scripts/rearrange_dirs.py
@@ -2,10 +2,10 @@ import subprocess
 import time
 import os
 
-def run_cmd(cmd, i):
-    print ('start running command {} for i={}'.format(cmd, i))
+def run_cmd(cmd):
+    # print ('start running command {} for i={}'.format(cmd, i))
     process = subprocess.call(cmd, shell=True)
-    print ('finished running command {} for i={}'.format(cmd, i))
+    # print ('finished running command {} for i={}'.format(cmd, i))
 
 # dump_file = '/data/gilad/logs/influence/cifar100/log_300419_b_125_wd_0.0004_mom_lr_0.1_f_0.9_p_3_c_2_val_size_1000_ls_0.01/test/dump.txt'
 # with open(dump_file) as f:
@@ -29,14 +29,14 @@ for i in range(10000):
     adv_dir = os.path.join(home_dir, 'test_index_{}'.format(i), 'adv')
     deepfool_dir = os.path.join(adv_dir, 'deepfool')
     if not os.path.exists(deepfool_dir):  # path not exist
-        run_cmd('mkdir -p ' + deepfool_dir, i)
+        run_cmd('mkdir -p ' + deepfool_dir)
         # go to dir
         os.chdir(adv_dir)
-        run_cmd('mv `find . -type f | grep -v "/cw/"`' + ' deepfool/', i)
+        run_cmd('mv `find . -type f | grep -v "/cw/"`' + ' deepfool/')
     else:  # path exist
         # check if empty
         if len(os.listdir(deepfool_dir)) == 0:  # indeed empty
             os.chdir(adv_dir)
-            run_cmd('mv `find . -type f | grep -v "/cw/"`' + ' deepfool/', i)
+            run_cmd('mv `find . -type f | grep -v "/cw/"`' + ' deepfool/')
 
 
