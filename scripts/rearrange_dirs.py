0181a6e7eefa8ab7febc95b1bcd06c58339dcb29
giladcohen <giladadlig@gmail.com>
2019-06-10 11:56:21 +0300

    no message

--- scripts/rearrange_dirs.py
+++ scripts/rearrange_dirs.py
@@ -24,7 +24,22 @@ def run_cmd(cmd):
 #     cmd = 'mv ' + os.path.join(dir, 'adv', '*') + ' ' + os.path.join(dir, 'adv', 'deepfool')
 #     run_cmd(cmd)
 
-val_inds = [  107,   127,   135,   150,   189,   255,   272,   292,   375,
+# home_dir = '/data/gilad/logs/influence/cifar10/log_080419_b_125_wd_0.0004_mom_lr_0.1_f_0.9_p_3_c_2_val_size_1000/val'
+# for i in cifar10_val_inds:
+#     adv_dir = os.path.join(home_dir, 'val_index_{}'.format(i), 'adv')
+#     deepfool_dir = os.path.join(adv_dir, 'deepfool')
+#     if not os.path.exists(deepfool_dir):  # path not exist
+#         run_cmd('mkdir -p ' + deepfool_dir)
+#         # go to dir
+#         os.chdir(adv_dir)
+#         run_cmd('mv `find . -type f | grep -v "/cw/"`' + ' deepfool/')
+#     else:  # path exist
+#         # check if empty
+#         if len(os.listdir(deepfool_dir)) == 0:  # indeed empty
+#             os.chdir(adv_dir)
+#             run_cmd('mv `find . -type f | grep -v "/cw/"`' + ' deepfool/')
+
+cifar10_val_inds = [  107,   127,   135,   150,   189,   255,   272,   292,   375,
          376,   385,   479,   512,   554,   730,   739,   760,   788,
          803,   810,   922,   971,  1136,  1219,  1258,  1285,  1347,
         1403,  1506,  1508,  1555,  1559,  1572,  1609,  1613,  1625,
@@ -138,18 +153,10 @@ val_inds = [  107,   127,   135,   150,   189,   255,   272,   292,   375,
        49997]
 
 home_dir = '/data/gilad/logs/influence/cifar10/log_080419_b_125_wd_0.0004_mom_lr_0.1_f_0.9_p_3_c_2_val_size_1000/val'
-for i in val_inds:
+for i in cifar10_val_inds:
     adv_dir = os.path.join(home_dir, 'val_index_{}'.format(i), 'adv')
-    deepfool_dir = os.path.join(adv_dir, 'deepfool')
-    if not os.path.exists(deepfool_dir):  # path not exist
-        run_cmd('mkdir -p ' + deepfool_dir)
-        # go to dir
+    fgsm_dir = os.path.join(adv_dir, 'fgsm')
+    if os.path.exists(fgsm_dir):
         os.chdir(adv_dir)
-        run_cmd('mv `find . -type f | grep -v "/cw/"`' + ' deepfool/')
-    else:  # path exist
-        # check if empty
-        if len(os.listdir(deepfool_dir)) == 0:  # indeed empty
-            os.chdir(adv_dir)
-            run_cmd('mv `find . -type f | grep -v "/cw/"`' + ' deepfool/')
-
+        run_cmd('mv fgsm fgsm_targeted')
 
