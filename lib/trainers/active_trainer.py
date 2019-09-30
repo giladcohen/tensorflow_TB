172fdd14fc321a1842ede0f01e57f02010d7a0c5
giladcohen <giladadlig@gmail.com>
2019-09-30 06:12:35 +0300

    no message

--- lib/trainers/active_trainer.py
+++ lib/trainers/active_trainer.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from lib.trainers.classification_trainer import ClassificationTrainer
+from tensorflow_TB.lib.trainers.classification_trainer import ClassificationTrainer
 from sklearn.decomposition import PCA
 import tensorflow as tf
 
